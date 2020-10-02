761d7469678a4fd612f7f6a6b017374ce75180a8
Omer Zulfiqar <omerzulfiqar@users.noreply.github.com>
2020-10-02 18:45:07 -0400

    material added

---
+++ src/containers/appContainer/appContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AppContainer extends Component {
+
+    render() {
+        return (
+            <div id='App-Container'>
+                <h1>Welcome To E-Commerce Solutions</h1>
+            </div>
+        )
+    }
+}
